d0c8eff5fef7d6e5eac33bde6f06147bd6d169c7
kunzheng <58841788+kunzms@users.noreply.github.com>
2020-03-02 10:54:59 -0800

    accessibility: elimate duplicate root id

--- src/index.tsx
+++ src/index.tsx
@@ -44,7 +44,7 @@ ReactDOM.render(
     <Provider store={store}>
         <App/>
     </Provider>
-    , document.getElementById("root"));
+    , document.getElementById("rootdiv"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
