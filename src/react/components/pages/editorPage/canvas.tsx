06bfc2bf60f4ccddf43f6c72dbd79c8461a34bbb
Tianxiang <v-tians@microsoft.com>
2020-03-16 14:49:46 -0700

    refactor: change sequence of items in commandbar

--- src/react/components/pages/editorPage/canvas.tsx
+++ src/react/components/pages/editorPage/canvas.tsx
@@ -1348,7 +1348,6 @@ export default class Canvas extends React.Component<ICanvasProps, ICanvasState> 
                 ocr.checkboxes.forEach((checkbox) => {
                     const checkboxFeature = this.createBoundingBoxVectorFeature(
                         checkbox.state, checkbox.boundingBox, imageExtent, ocrExtent, this.state.currentPage);
-                    console.log(checkboxFeature.getId());
                     this.regionOrders[ocr.page - 1][checkboxFeature.getId()] = order++;
                     this.regionOrderById[ocr.page - 1].push(checkboxFeature.getId());
                 });
@@ -1498,7 +1497,7 @@ export default class Canvas extends React.Component<ICanvasProps, ICanvasState> 
         newLayers[layer] = !newLayers[layer];
         this.setState({
             layers : newLayers,
-        });
+        }, () => console.log(this.state.layers));
     }
 
     private handleTableToolTipChange = async (display: string, width: number, height: number, top: number,
