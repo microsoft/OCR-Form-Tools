89e06763ec01732f52fee82943e8b388ec8ec540
Tianxiang <v-tians@microsoft.com>
2020-04-03 16:44:35 -0700

    fix: add bracket

--- src/react/components/pages/editorPage/canvas.tsx
+++ src/react/components/pages/editorPage/canvas.tsx
@@ -782,7 +782,7 @@ export default class Canvas extends React.Component<ICanvasProps, ICanvasState> 
             .find((r) => r.id === regionId).category === FeatureCategory.Checkbox)) {
                 selectedRegions.map((region) => this.removeFromSelectedRegions(region.id));
         } else if (category === FeatureCategory.Text ||
-            category === FeatureCategory.Label && this.state.currentAsset.regions
+            (category === FeatureCategory.Label && this.state.currentAsset.regions)
             .find((r) => r.id === regionId).category === FeatureCategory.Text) {
                 selectedRegions.filter((region) => region.category === FeatureCategory.Checkbox)
                     .map((region) => this.removeFromSelectedRegions(region.id));
