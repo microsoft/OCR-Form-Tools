1b907c1e7a935f99172c2b55f0ee96631638c034
alex-krasn <v-alexkr@microsoft.com>
2020-04-27 11:38:59 -0700

    merger conficts resolve

--- src/react/components/shell/statusBar.tsx
+++ src/react/components/shell/statusBar.tsx
@@ -15,7 +15,7 @@ export class StatusBar extends React.Component {
                     <ul>
                         <li>
                             <FontIcon iconName="BranchMerge" />
-                            <span>{appInfo.version}-b481f37</span>
+                            <span>{appInfo.version}-595a512</span>
                         </li>
                     </ul>
                 </div>
