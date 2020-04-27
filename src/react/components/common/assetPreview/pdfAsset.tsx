ee775ebff55e477ca94554f5b4a92ef58dcc28fb
stew-ro <v-stewro@microsoft.com>
2020-04-27 16:54:05 -0700

    fix: destroy unused pdf workers

--- src/react/components/common/assetPreview/pdfAsset.tsx
+++ src/react/components/common/assetPreview/pdfAsset.tsx
@@ -21,13 +21,26 @@ export interface IPDFAssetState {
  */
 export class PDFAsset extends React.Component<IAssetPreviewProps, IPDFAssetState> {
     private image: React.RefObject<HTMLImageElement> = React.createRef();
+    private pdf;
+    private page;
+    private renderTask;
+    private canvas;
+    private loadingTask;
 
     constructor(props: IAssetPreviewProps) {
         super(props);
-
+        this.page = null;
+        this.pdf = null;
+        this.renderTask = null;
+        this.canvas = null;
+        this.loadingTask = null;
         this.state = {
             imageUri: "",
         };
+    }
+
+    public componentWillUnmount() {
+        this.releaseMemoryUsedByPDF();
     }
 
     public componentDidMount() {
@@ -54,11 +67,13 @@ export class PDFAsset extends React.Component<IAssetPreviewProps, IPDFAssetState
     }
 
     private loadPdfFile = (url) => {
-        const loadingTask = pdfjsLib.getDocument(url);
-        loadingTask.promise.then((pdf) => {
+        this.loadingTask = pdfjsLib.getDocument(url);
+        this.loadingTask.promise.then((pdf) => {
+            this.pdf = pdf;
             // Fetch the first page
             this.loadPdfPage(pdf, 1 /*pageNumber*/);
         }, (reason) => {
+            this.releaseMemoryUsedByPDF();
             // PDF loading error
             if (this.props.onError) {
                 this.props.onError(reason);
@@ -68,14 +83,15 @@ export class PDFAsset extends React.Component<IAssetPreviewProps, IPDFAssetState
 
     private loadPdfPage = (pdf, pageNumber) => {
         pdf.getPage(pageNumber).then((page) => {
+            this.page = page;
             const defaultScale = 1;
             const viewport = page.getViewport({ scale: defaultScale });
 
             // Prepare canvas using PDF page dimensions
-            const canvas = document.createElement("canvas");
-            const context = canvas.getContext("2d");
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
+            this.canvas = document.createElement("canvas");
+            const context = this.canvas.getContext("2d");
+            this.canvas.height = viewport.height;
+            this.canvas.width = viewport.width;
 
             // Render PDF page into canvas context
             const renderContext = {
@@ -83,14 +99,20 @@ export class PDFAsset extends React.Component<IAssetPreviewProps, IPDFAssetState
                 viewport,
             };
 
-            const renderTask = page.render(renderContext);
-            renderTask.promise.then(() => {
-                const thumbnails = resizeCanvas(canvas, 240, 240).toDataURL(constants.convertedImageFormat, constants.convertedThumbnailQuality);
+            this.renderTask = page.render(renderContext);
+            this.renderTask.promise.then(() => {
+                const thumbnails = resizeCanvas(this.canvas, 240, 240).toDataURL(constants.convertedImageFormat, constants.convertedThumbnailQuality);
                 this.setState({
                     imageUri: thumbnails,
+                }, () => {
+                    this.releaseMemoryUsedByPDF();
                 });
+            }).catch((err) => {
+                this.releaseMemoryUsedByPDF();
             });
-          });
+        }).catch((err) => {
+            this.releaseMemoryUsedByPDF();
+        });
     }
 
     private onLoad = () => {
@@ -102,6 +124,39 @@ export class PDFAsset extends React.Component<IAssetPreviewProps, IPDFAssetState
         }
         if (this.props.onDeactivated) {
             this.props.onDeactivated(this.image.current);
+        }
+    }
+
+    private releaseMemoryUsedByPDF() {
+        if (this.pdf) {
+            this.pdf.cleanup();
+            if (!this.pdf.destroyed) {
+                this.pdf.destroy();
+            }
+            this.pdf = null;
+        }
+        if (this.renderTask) {
+            this.renderTask.cancel();
+            this.renderTask = null;
+        }
+        if (this.page) {
+            if (!this.page.pendingCleanup) {
+                this.page.cleanup();
+            }
+            if(!this.page.destroyed) {
+                this.page.destroy();
+            }
+            this.page = null;
+        }
+        if (this.loadingTask) {
+            if (!this.loadingTask.destroyed) {
+                this.loadingTask.destroy();
+            }
+            this.loadingTask = null;
+        }
+        if (this.canvas) {
+            delete this.canvas;
+            this.canvas = null;
         }
     }
 }
