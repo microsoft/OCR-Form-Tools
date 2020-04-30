a728703bdf7cf9bc808e3e8cede76c2e66b1ea6b
kunzheng <58841788+kunzms@users.noreply.github.com>
2020-04-30 13:39:40 -0700

    Update statusBar.tsx

--- src/react/components/shell/statusBar.tsx
+++ src/react/components/shell/statusBar.tsx
@@ -15,7 +15,7 @@ export class StatusBar extends React.Component {
                     <ul>
                         <li>
                             <FontIcon iconName="BranchMerge" />
-                            <span>{appInfo.version}-f068866</span>
+                            <span>{appInfo.version}-f3e42f6</span>
                         </li>
                     </ul>
                 </div>
