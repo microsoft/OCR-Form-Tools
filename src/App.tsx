c4e84417d5b2a8a55a4d413a13540ba06c6d8b5e
alex-krasn <v-alexkr@microsoft.com>
2020-05-14 10:48:43 -0700

    feature: draft of the feature

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import IAppErrorActions, * as appErrorActions from "./redux/actions/appErrorActi
 import { ErrorHandler } from "./react/components/common/errorHandler/errorHandler";
 import { KeyboardManager } from "./react/components/common/keyboardManager/keyboardManager";
 import { HelpMenu } from "./react/components/shell/helpMenu";
+import { HotKeysModal } from "./react/components/shell/hotKeysModal";
 import { MainContentRouter } from "./react/components/shell/mainContentRouter";
 import { Sidebar } from "./react/components/shell/sidebar";
 import { StatusBar } from "./react/components/shell/statusBar";
@@ -77,7 +78,12 @@ export default class App extends React.Component<IAppProps> {
                         <BrowserRouter>
                             <div className={`app-shell platform-${platform}`}>
                                 <TitleBar icon="TagGroup">
-                                    <div className="app-help-menu-icon"><HelpMenu/></div>
+                                <div className="app-hotkeys-menu-icon">
+                                    <HotKeysModal />
+                                </div>
+                                <div className="app-help-menu-icon">
+                                    <HelpMenu />
+                                </div>
                                 </TitleBar>
                                 <div className="app-main">
                                     <Sidebar project={this.props.currentProject} />
