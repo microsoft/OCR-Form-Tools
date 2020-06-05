cb7ffd4e04f73b37a7aaff082276d6a685aedcc7
alex-krasn <v-alexkr@microsoft.com>
2020-06-05 11:27:40 -0700

    refactor: delete unnecessary helper file

--- src/common/currPathParams.ts
+++
@@ -1,9 +0,0 @@
-import { matchPath } from "react-router-dom";
-
-
-export const getPathParams = (path: string, param: string) => {
-    const matchUrl = matchPath(path, {
-      path: `/projects/:${param}`,
-    });
-    return (matchUrl && matchUrl.params) || {};
-}
