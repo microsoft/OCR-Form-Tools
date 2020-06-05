71a3fb28961481bacb4cafdf4f1554f3f55bec4f
Joel Ye <joelye9@gmail.com>
2020-06-05 17:51:32 -0400

    fix: Format a little better

--- src/react/components/common/generators/generateStrings.tsx
+++ src/react/components/common/generators/generateStrings.tsx
@@ -4,7 +4,8 @@ import * as RandExp from "randexp";
 
 import { IGenerator, FieldFormat, FieldType } from "../../../../models/applicationState";
 
-export const generateString: (g: IGenerator) => string = (generator) => {
+export const generateString: (g: IGenerator, maxLength?: number) => string = (generator) => {
+    // TODO incorporate max length
     const fieldType = generator.type;
     const fieldFormat = generator.format;
     let regex = regexDict[FieldType.String][FieldFormat.NotSpecified];
@@ -18,7 +19,7 @@ export const generateString: (g: IGenerator) => string = (generator) => {
 }
 
 const low = 2;
-const high = 10;
+const high = 6;
 const defaultRegex = `^.{${low},${high}}$`;
 const dd = "(0[1-9]|[12][0-9]|3[01])";
 const mm = "(0[1-9]|1[012])";
@@ -30,11 +31,11 @@ const regexDict = {
         [FieldFormat.NoWhiteSpaces]: `^[a-zA-Z0-9]{${low},${high}}$`,
     },
     [FieldType.Number]: {
-        [FieldFormat.NotSpecified]: `^\d{${low},${high}}$`,
-        [FieldFormat.Currency]: "^\$?([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(\.[0-9][0-9])?$",
+        [FieldFormat.NotSpecified]: `^\\d{${low},${high}}$`,
+        [FieldFormat.Currency]: `^\\$?([0-9]{1,3},([0-9]{3},){${low},${3}}[0-9]{3}|[0-9]{${low},${3}})(\\.[0-9][0-9])?$`,
     },
     [FieldType.Date]: {
-        [FieldFormat.NotSpecified]: `^\d\d[- /.]\d\d[- /.]\d{2,4}$
+        [FieldFormat.NotSpecified]: `^\\d\\d[- /.]\\d\\d[- /.]\\d{2,4}$
         `,
         [FieldFormat.DMY]: `^${dd}[- /.]${mm}[- /.]${yy}$`,
         [FieldFormat.MDY]: `^${mm}[- /.]${dd}[- /.]${yy}$`,
@@ -44,7 +45,7 @@ const regexDict = {
         [FieldFormat.NotSpecified]: "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$",
     },
     [FieldType.Integer]: {
-        [FieldFormat.NotSpecified]: `^\d{${low},${high}}$`,
+        [FieldFormat.NotSpecified]: `^\\d{${low},${high}}$`,
     },
     [FieldType.SelectionMark]: {
         // no support
