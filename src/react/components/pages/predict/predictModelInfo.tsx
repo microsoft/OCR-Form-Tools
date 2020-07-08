abc63767e97dd28a6bb9028e03f2225e6ac0f1ab
alex-krasn <64093224+alex-krasn@users.noreply.github.com>
2020-07-08 14:29:05 -0700

    feat: enable new model info in preditct page (#383)
    * feat: enable new model info in preditc page
    
    * refactor styles and data
    
    * fix: indents and semicolons

---
+++ src/react/components/pages/predict/predictModelInfo.tsx
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import React from 'react'
+import './predictModelInfo.scss';
+
+export default function PredictModelInfo({ modelInfo }) {
+    const { docType, modelId, docTypeConfidence } = modelInfo;
+    const getPercentage = (value: number) => {
+        const percents = 100 * value;
+        return percents % 1 !== 0 ? percents.toFixed(2) : percents;
+    };
+    const modeType = docType.split(":")[0];
+    const modelName = docType.split(":")[1];
+
+    return (
+        <div className="model-info-container">
+            <div className="model-info-item">
+                <span className="title" >Model type:</span>
+                <span className="value" >{modeType}</span>
+            </div>
+            {
+                modelName !== modelId &&
+                <div className="model-info-item">
+                    <span className="title" >Model name:</span>
+                    <span className="value" >{modelName}</span>
+                </div>
+            }
+            <div className="model-info-item">
+                <span className="title" >Model id:</span>
+                <span className="value" >{modelId}</span>
+            </div>
+            <div className="model-info-item">
+                <span className="title" >Document type confidence:</span>
+                <span className="value" >{getPercentage(docTypeConfidence)}%</span>
+            </div>
+        </div>
+    )
+}
