90698f36f4de41c3adb9198bfb9e0ed1f591a1c0
alex-krasn <v-alexkr@microsoft.com>
2020-07-09 12:23:27 -0700

    - unused import

--- src/react/components/pages/modelCompose/composeModelView.tsx
+++ src/react/components/pages/modelCompose/composeModelView.tsx
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import React from "react";
-import { Customizer, IColumn, ICustomizations, Modal, DetailsList, SelectionMode, DetailsListLayoutMode, PrimaryButton, TextField, VerticalDivider } from "@fluentui/react";
+import { Customizer, IColumn, ICustomizations, Modal, DetailsList, SelectionMode, DetailsListLayoutMode, PrimaryButton, TextField } from "@fluentui/react";
 import { getDarkGreyTheme, getPrimaryGreenTheme, getPrimaryRedTheme } from "../../../../common/themes";
 import { strings } from "../../../../common/strings";
 import { IModel } from "./modelCompose";
