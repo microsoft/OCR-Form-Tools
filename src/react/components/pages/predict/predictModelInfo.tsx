f46c1c5cf8cffc5628d7d2167777f8046a5ad783
Robert Stewart (eXcell <v-stewro@microsoft.com>
2020-07-13 10:56:03 -0700

    fix: make analyzed result UI key-values same as json key-values

--- src/react/components/pages/predict/predictModelInfo.tsx
+++ src/react/components/pages/predict/predictModelInfo.tsx
@@ -6,33 +6,19 @@ import './predictModelInfo.scss';
 
 export default function PredictModelInfo({ modelInfo }) {
     const { docType, modelId, docTypeConfidence } = modelInfo;
-    const getPercentage = (value: number) => {
-        const percents = 100 * value;
-        return percents % 1 !== 0 ? percents.toFixed(2) : percents;
-    };
-    const modeType = docType.split(":")[0];
-    const modelName = docType.split(":")[1];
-
     return (
         <div className="model-info-container">
             <div className="model-info-item">
-                <span className="title" >Model type:</span>
-                <span className="value" >{modeType}</span>
+                <span className="title" >docType:</span>
+                <span className="value" >{docType}</span>
             </div>
-            {
-                modelName !== modelId &&
-                <div className="model-info-item">
-                    <span className="title" >Model name:</span>
-                    <span className="value" >{modelName}</span>
-                </div>
-            }
             <div className="model-info-item">
-                <span className="title" >Model id:</span>
+                <span className="title" >modelId:</span>
                 <span className="value" >{modelId}</span>
             </div>
             <div className="model-info-item">
-                <span className="title" >Document type confidence:</span>
-                <span className="value" >{getPercentage(docTypeConfidence)}%</span>
+                <span className="title" >docTypeConfidence:</span>
+                <span className="value" >{docTypeConfidence}</span>
             </div>
         </div>
     )
