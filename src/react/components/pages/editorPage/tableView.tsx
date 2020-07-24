dbb9305741214d1e662122ddbb383d45a64089ff
stew-ro <v-stewro@microsoft.com>
2020-07-24 01:05:46 -0700

    refactor: remove trailing whitespace

--- src/react/components/pages/editorPage/tableView.tsx
+++ src/react/components/pages/editorPage/tableView.tsx
@@ -44,7 +44,7 @@ export const TableView: React.FunctionComponent<ITableViewProps> = (props) => {
         }
         return tableBody;
     }
-    
+
     return (
         <Customizer {...dark}>
             <Modal
