55be5427e4a2f9c8cf393d446049527c55f841d4
alex-krasn <64093224+alex-krasn@users.noreply.github.com>
2020-07-24 13:33:54 -0700

    feat: add apiVersion to telemetry (#448)
    * feat: add apiVersion
    
    * fix: key
    
    * refactor: move insights apiVersion into telemetryServise.ts

--- src/react/components/pages/train/trainPage.tsx
+++ src/react/components/pages/train/trainPage.tsx
@@ -256,7 +256,7 @@ export default class TrainPage extends React.Component<ITrainPageProps, ITrainPa
             });
         });
         if (this.appInsights) {
-            this.appInsights.trackEvent({ name: "TRAIN_MODEL_EVENT" });
+            this.appInsights.trackEvent({name: "TRAIN_MODEL_EVENT"});
         }
     }
 
