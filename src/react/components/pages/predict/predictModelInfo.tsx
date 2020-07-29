f00cac2e58134fe5dced02d85a636d653bec5c36
Alex Chen <v-yongbc@microsoft.com>
2020-07-29 15:34:58 +0800

    remove docType from predicatModelInfo

--- src/react/components/pages/predict/predictModelInfo.tsx
+++ src/react/components/pages/predict/predictModelInfo.tsx
@@ -5,13 +5,9 @@ import React from 'react'
 import './predictModelInfo.scss';
 
 export default function PredictModelInfo({ modelInfo }) {
-    const { docType, modelId, docTypeConfidence } = modelInfo;
+    const { modelId, docTypeConfidence } = modelInfo;
     return (
         <div className="model-info-container">
-            <div className="model-info-item">
-                <span className="title" >docType:</span>
-                <span className="value" >{docType}</span>
-            </div>
             <div className="model-info-item">
                 <span className="title" >modelId:</span>
                 <span className="value" >{modelId}</span>
