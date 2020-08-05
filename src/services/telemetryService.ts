d44395e767026b36d6774c0d287ddf74806850a7
alex-krasn <v-alexkr@microsoft.com>
2020-08-05 12:52:26 -0700

    add all page names

--- src/services/telemetryService.ts
+++ src/services/telemetryService.ts
@@ -1,21 +1,28 @@
-import {ApplicationInsights, ITelemetryItem} from '@microsoft/applicationinsights-web';
-import {ReactPlugin} from '@microsoft/applicationinsights-react-js';
-import {constants} from '../common/constants';
+import { ApplicationInsights, ITelemetryItem } from "@microsoft/applicationinsights-web";
+import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+import { constants } from "../common/constants";
 
 let reactPlugin = null;
 let appInsights = null;
 
 const adjustPageViewName = (item) => {
-    const route: string = item.uri.substr (item.uri.lastIndexOf ( "/" ) + 1)
-    switch (route) {
-        case "edit":
+    const pathArray: string[] = item.uri.split("/");
+    const pathName = pathArray.length > 2 ? pathArray[1].slice(0, -1) + "_" + pathArray[3] : pathArray[1];
+    switch (pathName) {
+        case "project_edit":
             return "Editor";
-        case "train":
+        case "project_train":
             return "Train";
-        case "modelcompose":
+        case "project_modelcompose":
             return "Model_Compose"
-        case "predict":
+        case "project_predict":
             return "Analyze";
+        case "project_settings":
+            return "Project_Setting";
+        case "connections":
+            return "Application_Connections";
+        case "settings":
+            return "Application_Settings";
         default:
             return "Home";
     }
@@ -30,15 +37,15 @@ const createTelemetryService = () => {
     /**
      * Initialize the Application Insights class
      * @param {string} instrumentationKey - Application Insights Instrumentation Key
-     * @param {Object} browserHistory - client's browser history, supplied by the withRouter HOC
+     * @param {Object} browserHistory - client"s browser history, supplied by the withRouter HOC
      * @return {void}
      */
     const initialize = (instrumentationKey: string, browserHistory: any): void => {
         if (!browserHistory) {
-            throw new Error('Could not initialize Telemetry Service');
+            throw new Error("Could not initialize Telemetry Service");
         }
         if (!instrumentationKey) {
-            throw new Error('Telemetry Service Instrumentation key not provided.')
+            throw new Error("Telemetry Service Instrumentation key not provided.")
         }
 
         reactPlugin = new ReactPlugin();
@@ -60,13 +67,13 @@ const createTelemetryService = () => {
         appInsights.loadAppInsights();
 
         appInsights.context.application.ver = constants.apiVersion;
-        appInsights.addTelemetryInitializer((envelope)=>{
+        appInsights.addTelemetryInitializer((envelope) => {
             const telemetryItem: ITelemetryItem = envelope.baseData;
             telemetryItem.name = adjustPageViewName(telemetryItem);
         })
     };
 
-    return {reactPlugin, appInsights, initialize};
+    return { reactPlugin, appInsights, initialize };
 };
 
 export const ai = createTelemetryService();
