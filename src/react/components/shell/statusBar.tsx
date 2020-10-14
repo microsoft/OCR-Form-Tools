c9b505d1e3c08f62d7fe1923fb54e7b7f05c257c
Alex Chen <v-yongbc@microsoft.com>
2020-10-14 12:47:17 +0800

    get current commit hash-code

--- src/react/components/shell/statusBar.tsx
+++ src/react/components/shell/statusBar.tsx
@@ -2,11 +2,22 @@
 // Licensed under the MIT license.
 
 import React from "react";
-import { FontIcon } from "@fluentui/react";
-import { constants } from "../../../common/constants";
+import {FontIcon} from "@fluentui/react";
+import {constants} from "../../../common/constants";
+import axios from 'axios';
 import "./statusBar.scss";
 
-export class StatusBar extends React.Component {
+interface IStatusBarState {
+    commitHash?: string;
+}
+export class StatusBar extends React.Component<{}, IStatusBarState> {
+    componentDidMount() {
+        const commitInfoUrl = require("../../../git-commit-info.txt");
+        axios.get(commitInfoUrl).then(res => {
+            const commitHash = /commit (\S{8})/.exec(res.data)[1];
+            this.setState({commitHash});
+        });
+    }
     public render() {
         return (
             <div className="status-bar">
@@ -16,7 +27,7 @@ export class StatusBar extends React.Component {
                         <li>
                             <a href="https://github.com/microsoft/OCR-Form-Tools/blob/master/CHANGELOG.md" target="blank" rel="noopener noreferrer">
                                 <FontIcon iconName="BranchMerge" />
-                                <span>{constants.appVersion}-1f33130</span>
+                                <span>{constants.appVersion}-{this.state?.commitHash}</span>
                             </a>
                         </li>
                     </ul>
