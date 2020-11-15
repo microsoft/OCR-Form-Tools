c7e811e9a2a20d02becaaf6638fb5e0ab247c13f
Mick Vleeshouwer <mick@imick.nl>
2020-11-15 15:47:42 +0100

    Simplify if statement
    Signed-off-by: Mick Vleeshouwer <mick@imick.nl>

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -127,7 +127,7 @@ app.on("ready", createWindow);
 app.on("window-all-closed", () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== "darwin") {
+    if (!isMac) {
         app.quit();
     }
 });
