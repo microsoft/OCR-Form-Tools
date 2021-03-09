65baba04ac06323767f7a4f01660ceecbe97816e
Buddha Wang <shihw@microsoft.com>
2021-03-09 15:24:49 +0800

    Add toast for displaying error messages.

--- src/common/webStorage.ts
+++ src/common/webStorage.ts
@@ -1,7 +1,8 @@
 import * as localforage from "localforage";
+import { toast } from "react-toastify";
 
 class WebStorage {
-    private driverIsReady: boolean = false;
+    private isStorageReady: boolean = false;
 
     constructor() {
         localforage.config({
@@ -10,59 +11,41 @@ class WebStorage {
         });
     }
 
-    private async checkDriverIsReady() {
-        if (!this.driverIsReady) {
+    private async waitForStorageReady() {
+        if (!this.isStorageReady) {
             try {
                 await localforage.ready();
-                this.driverIsReady = true;
+                this.isStorageReady = true;
             } catch (error) {
-                console.error(`No usable storage driver is found.`);
+                toast.error(`No usable storage driver is found.`);
             }
-        }
-    }
-
-    private async checkVolume() {
-        if (navigator.storage && navigator.storage.estimate) {
-            const { quota, usage } = await navigator.storage.estimate();
-            const percenttageUsed = (usage / quota) * 100;
-            console.log(
-                `You've used ${percenttageUsed}% of the available storage.`
-            );
-            const remaining = quota - usage;
-            console.log(`You can write up to ${remaining} more bytes.`);
         }
     }
 
     public async getItem(key: string) {
         try {
+            await this.waitForStorageReady();
             return await localforage.getItem(key);
         } catch (err) {
-            console.error(
-                `webstorage called getItem with key ${key} error`,
-                err
-            );
+            toast.error(`WebStorage getItem("${key}") error: ${err}`);
         }
     }
 
     public async setItem(key: string, value: any) {
         try {
+            await this.waitForStorageReady();
             await localforage.setItem(key, value);
         } catch (err) {
-            console.error(
-                `webstorage called setItem with key: ${key}, value: ${value},  error`,
-                err
-            );
+            toast.error(`WebStorage setItem("${key}") error: ${err}`);
         }
     }
 
     public async removeItem(key: string) {
         try {
+            await this.waitForStorageReady();
             await localforage.removeItem(key);
         } catch (err) {
-            console.error(
-                `webstorage called removeItem with key: ${key},  error`,
-                err
-            );
+            toast.error(`WebStorage removeItem("${key}") error: ${err}`);
         }
     }
 }
