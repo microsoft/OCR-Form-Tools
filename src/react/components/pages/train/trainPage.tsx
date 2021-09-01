f61b06fc73d4f8f2b36af16daaa23f5dce042591
v-yuhang <v-yuhangzhao@microsoft.com>
2021-09-01 09:45:55 +0800

    timeOutchange

--- src/react/components/pages/train/trainPage.tsx
+++ src/react/components/pages/train/trainPage.tsx
@@ -411,7 +411,7 @@ export default class TrainPage extends React.Component<ITrainPageProps, ITrainPa
             const result = await ServiceHelper.postWithAutoRetry(
                 baseURL,
                 payload,
-                {timeout:5400000},
+                {},
                 this.props.project.apiKey as string,
             );
             this.setState({ modelUrl: result.headers.location });
@@ -465,7 +465,7 @@ export default class TrainPage extends React.Component<ITrainPageProps, ITrainPa
 
     private async getTrainStatus(operationLocation: string): Promise<any> {
         const timeoutPerFileInMs = 10000;  // 10 second for each file
-        const minimumTimeoutInMs = 10 * 60 * 1000;  // 10 minutes minimum waiting time for each training process.
+        const minimumTimeoutInMs = 90 * 60 * 1000;  // 90 minutes minimum waiting time for each training process.
         const extendedTimeoutInMs = timeoutPerFileInMs * Object.keys(this.props.project.assets || []).length;
         const res = this.poll(() => {
             return ServiceHelper.getWithAutoRetry(
