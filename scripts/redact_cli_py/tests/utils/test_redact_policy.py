2b130424f76cab5d1391a7c90674a9accf2890c3
Chia-Sheng Chen <mail@cschen.io>
2021-12-14 16:36:33 +0800

    Redaction Toolkit | Support to redact some Latin ligature letters and letters with diacritics (#1012)
    ## Purpose
    
    We currently cannot handle accented letter like é and ligature letter like œ, which is common in French. Need to support this. We also expand the language coverage out of just French. Please check the code for the actual charset we have taken care of.
    
    ## Validation
    
    Before merging this PR, please make sure below works are done and marked items with 'x'.
    
    - [x] Your code builds clean without any errors or warnings.
    - [x] You have tested your change manually.
    - [x] You have added unit tests.
    
    Co-authored-by: Chia-Sheng Chen <chiache@microsoft.com>

--- scripts/redact_cli_py/tests/utils/test_redact_policy.py
+++ scripts/redact_cli_py/tests/utils/test_redact_policy.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT License. See License.txt in the project
 # root for license information.
 
-from redact.utils.redact_policy import first_char
+from redact.utils.redact_policy import first_char, remove_diacritics
 
 
 class TestRedactPolicy:
@@ -25,3 +25,38 @@ class TestRedactPolicy:
         text = "$3000.00"
         actual = first_char(text)
         assert "$0000.00" == actual
+
+    def test_first_char_diacritics(self) -> None:
+        text = "Anaïs, Noël, Sørina, François, Mátyás, Agnès, Fañch, Reiß"
+        actual = first_char(text)
+        assert "Aaaaa, Aaaa, Aaaaaa, Aaaaaaaa, Aaaaaa, Aaaaa, Aaaaa, Aaaa" == actual
+
+    def test_remove_diacritics_empty(self) -> None:
+        text = ""
+        actual = remove_diacritics(text)
+        assert "" == actual
+
+    def test_remove_diacritics_with_diacritics(self) -> None:
+        text = "Português, Lô-má-jī"
+        actual = remove_diacritics(text)
+        assert "Portugues, Lo-ma-ji" == actual
+
+    def test_remove_diacritics_french_letters(self) -> None:
+        text = "çéâêîôûàèìòùëïü"
+        actual = remove_diacritics(text)
+        assert "ceaeiouaeioueiu" == actual
+
+    def test_remove_diacritics_typographical_ligature(self) -> None:
+        text = "ﬀﬃﬄﬁﬂﬆﬅ"
+        actual = remove_diacritics(text)
+        assert "ffffifflfiflstst" == actual
+
+    def test_remove_diacritics_linguistic_ligature(self) -> None:
+        text = "ꜳæꬱꜵꜷꜹꜻꜽ🙰ꭁƕỻœꝏßꜩꝡ"
+        actual = remove_diacritics(text)
+        assert text == actual
+
+    def test_remove_diacritics_boeuf_a_la_bourguignonne(self) -> None:
+        text = "bœuf à la Bourguignonne"
+        actual = remove_diacritics(text)
+        assert "bœuf a la Bourguignonne" == actual
