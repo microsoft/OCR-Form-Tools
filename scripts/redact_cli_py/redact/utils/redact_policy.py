1752ca1cb14a157bd623d7e9d9bca9ae0cf15e97
Chia-Sheng Chen <chiache@microsoft.com>
2021-12-14 05:54:49 +0000

    Merged PR 5465603: Redaction Toolkit | Support to redact some Latin ligature letters and letters with diacritics
    ## Purpose
    
    We currently cannot handle accented letter like é and ligature letter like œ, which is common in French. Need to support this. We also expand the language coverage out of just French. Please check the code for the actual charset we have taken care of.
    
    ## Validation
    
    Before merging this PR, please make sure below works are done and marked items with 'x'.
    
    - [x] Your code builds clean without any errors or warnings.
    - [x] You have tested your change manually.
    - [x] You have added unit tests.
    
    Related work items: #12827267

--- scripts/redact_cli_py/redact/utils/redact_policy.py
+++ scripts/redact_cli_py/redact/utils/redact_policy.py
@@ -3,12 +3,37 @@
 # root for license information.
 
 import re
+import unicodedata
 
 
 def first_char(item: str) -> str:
     # This replace every uppercase to 'A', lowercase to 'a', digit to '0'.
     # As known as the "Aa0" policy.
-    ret = re.sub('[A-Z]', 'A', item)
-    ret = re.sub('[a-z]', 'a', ret)
+
+    # First remove all diacritics and break typographical ligatures.
+    ret = remove_diacritics(item)
+
+    # This also takes care of other common letter in Europe languages (Ø) and
+    # linguistic ligatures (Œ) instead of just A-Z.
+    ret = re.sub('[A-ZØÞŁꜲÆꜴꜶꜸꜺꜼǶŒꝎẞꜨꝠ]', 'A', ret)
+    ret = re.sub('[a-zøþıłꜳæꬱꜵꜷꜹꜻꜽ🙰ꭁƕỻœꝏßꜩꝡ]', 'a', ret)
     ret = re.sub('[0-9]', '0', ret)
     return ret
+
+
+def remove_diacritics(input_str: str) -> str:
+    """Remove diacritics and typographical ligatures from the string.
+
+    - All diacritics (i.e. accents) will be removed.
+    - Typographical ligatures (e.g. ﬃ) are broken into separated characters.
+    - True linguistic ligatures (e.g. œ) will remain.
+    - Non-latin scripts will remain.
+
+    Args:
+        input_str (str): The original string with diacritics and ligatures.
+
+    Returns:
+        str: The string without diacritics and typographical ligatures.
+    """
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
